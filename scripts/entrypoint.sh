631e8af160cd6696a9c37c84b682dc14162c7f55
derekdon <derek@codex9.com>
2016-10-21 01:38:02 +0100

    Added bucket creation

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -23,6 +23,31 @@ set -e
         exit 4
     fi
     
+    if [ -z "$BUCKET" ]; then
+        echo "BUCKET environment variable not set"
+        exit 5
+    fi
+    
+    if [ -z "$BUCKET_TYPE" ]; then
+        echo "BUCKET_TYPE environment variable not set"
+        exit 6
+    fi
+        
+    if [ -z "$BUCKET_PORT" ]; then
+        echo "BUCKET_PORT environment variable not set"
+        exit 7
+    fi 
+        
+    if [ -z "$BUCKET_RAM_SIZE" ]; then
+        echo "BUCKET_RAM_SIZE environment variable not set"
+        exit 8
+    fi 
+    
+    if [ -z "$BUCKET_REPLICA" ]; then
+        echo "BUCKET_REPLICA environment variable not set"
+        exit 9
+    fi
+                  
     echo "initializing cluster ($CLUSTER)"
     couchbase-cli cluster-init \
         -c "$CLUSTER" \
@@ -31,6 +56,15 @@ set -e
         "--cluster-init-ramsize=$RAM_SIZE"
     
     echo "cluster initialized"
+    echo "creating bucket"
+    
+    couchbase-cli bucket-create \
+        -c "$CLUSTER" \
+        -u "$USERNAME" \
+        -p "$PASSWORD" \
+        "--bucket=$BUCKET --bucket-type=$BUCKET_TYPE --bucket-port=$BUCKET_PORT --bucket-ramsize=$BUCKET_RAM_SIZE --bucket-replica=$BUCKET_REPLICA"
+    
+    echo "created ($BUCKET) bucket"
 
 }
 
