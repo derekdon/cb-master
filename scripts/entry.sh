c20c376cfb771523d5e3652eddc18891c018db9e
derekdon <derek@codex9.com>
2016-11-18 01:49:57 +0000

    removing the need for cluster env var

--- scripts/entry.sh
+++ scripts/entry.sh
@@ -3,30 +3,22 @@ set -e
 set -x
 set -m
 
-if [ -z "$CLUSTER" ]; then
-    echo "CLUSTER environment variable not set"
-    exit 1
-fi
-
 if [ -z "$CLUSTER_PORT" ]; then
     echo "CLUSTER_PORT environment variable not set"
-    exit 2
+    exit 1
 fi
 
 if [ -z "$USERNAME" ]; then
     echo "USERNAME environment variable not set"
-    exit 3
+    exit 2
 fi
 
 if [ -z "$PASSWORD" ]; then
     echo "PASSWORD environment variable not set"
-    exit 4
+    exit 3
 fi
 
 /entrypoint.sh couchbase-server &
-
-CLUSTER_IP=$(getent hosts $CLUSTER | awk '{print $1}' | sort -n | head -1)
-echo "CLUSTER_IP $CLUSTER_IP"
 
 FIRST_IP=$(getent hosts $DOCKERCLOUD_SERVICE_HOSTNAME | awk '{print $1}' | sort -n | head -1)
 echo "FIRST_IP in the service is $FIRST_IP"
@@ -37,10 +29,14 @@ echo "LAST_IP in the service is $LAST_IP"
 THIS_IP=$(getent hosts $DOCKERCLOUD_CONTAINER_HOSTNAME | awk '{print $1}' | sort -n | head -1)
 echo "THIS_IP $THIS_IP"
 
-if [ -z "$CLUSTER_IP" ]; then
-    echo "Unable to get CLUSTER_IP via getent for ${CLUSTER}"
-    exit 5
+if [ -z "$CLUSTER" ]
+then
+   CLUSTER_IP=$FIRST_IP
+else
+   CLUSTER_IP=$(getent hosts $CLUSTER | awk '{print $1}' | sort -n | head -1)
 fi
+
+echo "CLUSTER_IP $CLUSTER_IP"
 
 CLUSTER_IP_PORT="${CLUSTER_IP}:$CLUSTER_PORT"
 echo "CLUSTER_IP_PORT $CLUSTER_IP_PORT"
@@ -52,37 +48,37 @@ then
 
     if [ -z "$CLUSTER_RAM_SIZE" ]; then
         echo "CLUSTER_RAM_SIZE environment variable not set"
-        exit 6
+        exit 4
     fi
     
     if [ -z "$CLUSTER_INDEX_RAM_SIZE" ]; then
         echo "CLUSTER_INDEX_RAM_SIZE environment variable not set"
-        exit 7
+        exit 5
     fi
     
     if [ -z "$BUCKET" ]; then
         echo "BUCKET environment variable not set"
-        exit 8
+        exit 6
     fi
     
     if [ -z "$BUCKET_TYPE" ]; then
         echo "BUCKET_TYPE environment variable not set"
-        exit 9
+        exit 7
     fi
         
     if [ -z "$BUCKET_PORT" ]; then
         echo "BUCKET_PORT environment variable not set"
-        exit 10
+        exit 8
     fi 
         
     if [ -z "$BUCKET_RAM_SIZE" ]; then
         echo "BUCKET_RAM_SIZE environment variable not set"
-        exit 11
+        exit 9
     fi 
     
     if [ -z "$BUCKET_REPLICA" ]; then
         echo "BUCKET_REPLICA environment variable not set"
-        exit 12
+        exit 10
     fi
     
     set +e
@@ -133,16 +129,16 @@ else
 
   if [ -z "$SERVER_ADD_HOST" ]; then
       echo "SERVER_ADD_HOST environment variable not set"
-      exit 6
+      exit 4
   fi
   
   sleep 15
 
   echo "joining cluster ($CLUSTER_IP_PORT) from ($SERVER_ADD_HOST)"
   
-  echo "auto rebalance ($AUTO_REBALANCE)"
+  echo "auto rebalance ($AUTO_REBALANCE), note only the last container can trigger a rebalance"
       
-  if [ $THIS_IP == $LAST_IP && "$AUTO_REBALANCE" = "true" ]; then
+  if [ $THIS_IP == $LAST_IP ] && [ "$AUTO_REBALANCE" = "true" ]; then
       couchbase-cli rebalance -c $CLUSTER_IP_PORT -u $USERNAME -p $PASSWORD --server-add=$SERVER_ADD_HOST --server-add-username=$USERNAME --server-add-password=$PASSWORD --services=data,index,query
   else
       couchbase-cli server-add -c $CLUSTER_IP_PORT -u $USERNAME -p $PASSWORD --server-add=$SERVER_ADD_HOST --server-add-username=$USERNAME --server-add-password=$PASSWORD --services=data,index,query
